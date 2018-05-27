933d213746db3c2a70c5610785163016c6c62ffa
lfvm2410 <luis_f_94@hotmail.com>
2018-05-26 18:52:15 -0600

    Creación del proyecto

---
+++ webBancoMVC/webBancoMVC/Models/EntidadClientes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+public class EntidadClientes
+{
+    [Display(Name = "ID")] // Data annotation Para visualizacion, se despliega el nombre "ID"
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public string ClienteID { get; set; }
+    [Display(Name = "ID")]
+    [Required(ErrorMessage = "Debe digitar el tipo de cliente")]
+    public string TipoClienteID { get; set; }
+    [Required(ErrorMessage = "Debe digitar el nombre del cliente")]
+    public string Nombre { get; set; }
+    [Required(ErrorMessage = "Debe digitar los apellidos del cliente")]
+    public string Apellidos { get; set; }
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public string Direccion { get; set; }
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public string Telefono { get; set; }
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public string Email { get; set; }
+    [Display(Name = "ID")]
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public DateTime Fecha_Ingreso { get; set; }
+    [Display(Name = "ID")]
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public string Sexo { get; set; }
+    [Display(Name = "ID")]
+    [Required(ErrorMessage = "Debe digitar el ID del cliente")]
+    public string Clave { get; set; }
+
+}
